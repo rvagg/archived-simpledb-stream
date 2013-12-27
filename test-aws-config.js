91621856d323209f9ad7c4f52c22735a44701ddf
Rod Vagg <rod@vagg.org>
2013-12-27 17:49:37 +1100

    travis take 5

--- test-aws-config.js
+++ test-aws-config.js
@@ -5,7 +5,20 @@ try {
 } catch (e) {
   try {
     config = JSON.parse(process.env.AWS_CONFIG)
-  } catch (e) {}
+  } catch (e) {
+    try {
+      var _config = {
+          accessKeyId     : process.env.AWS_ACCESS_KEY_ID
+        , secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
+        , region          : process.env.AWS_REGION
+      }
+      if (typeof _config.accessKeyId == 'string'
+          && typeof _config.secretAccessKey == 'string'
+          && typeof _config.region == 'string') {
+        config = _config
+      }
+    } catch (e) {}
+  }
 }
 
 if (typeof config != 'object')
